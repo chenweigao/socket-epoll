d653e27c422a61db1cd2caf831d1ad3098a4e2e9
chenweigao <297859260@qq.com>
2019-06-03 14:34:43 +0800

    update epoll nonblock et model

--- cpp_webserver/nonblock_et_epoll.c
+++ cpp_webserver/nonblock_et_epoll.c
@@ -6,7 +6,8 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <ctype.h>
-
+#include <fcntl.h>
+#include <errno.h>
 
 int main(int argc, const char* argv[])
 {
@@ -18,19 +19,18 @@ int main(int argc, const char* argv[])
 	struct sockaddr_in serv_addr;
 	socklen_t serv_len = sizeof(serv_addr);
 	int port = atoi(argv[1]);
-	printf("%d", port);
+	printf("server at port: [%d]\n", port);
 
 	int lfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	memset(&serv_addr, 0, serv_len);
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	// 地址族
 	// or serv_addr.sin_addr.s_addr = inet_addr("192.168.1.1");
-	serv_addr.sin_port = htons(port); // 这两个用于转化大端或者小端
+	serv_addr.sin_port = htons(port);
 
 	bind(lfd, (struct sockaddr*) & serv_addr, serv_len);
-	listen(lfd, 36); // 设置监听的最大个数
+	listen(lfd, 36);
 	printf("Start accept...\n");
 
 	struct sockaddr_in client_addr;
@@ -40,24 +40,20 @@ int main(int argc, const char* argv[])
 
 	struct epoll_event ev;
 	ev.events = EPOLLIN;
-	// 设置边沿触发模式
-	// temp.events = EPOLLIN | EPOLLET;
 	ev.data.fd = lfd;
+
 	epoll_ctl(epfd, EPOLL_CTL_ADD, lfd, &ev);
 
 	struct epoll_event all[2000];
+
 	while (1)
 	{
-		// epoll 实现
-		// 第二个参数为传出参数
 		int ret = epoll_wait(epfd, all, sizeof(all) / sizeof(all[0]), -1);
 		printf("ret: %d\n", ret);
 		for (int i = 0; i < ret; i++)
 		{
-			// 遍历所有可用的 socket 连接，all 数组中的前 ret 个元素
 			int fd = all[i].data.fd;
 
-			// 有新连接
 			if (fd == lfd)
 			{
 				int cfd = accept(lfd, (struct sockaddr*) & client_addr, &cli_len);
@@ -66,15 +62,18 @@ int main(int argc, const char* argv[])
 					perror("accept error");
 					exit(1);
 				}
-				// 将 cfd 挂到 rbtree
+
+				int flag = fcntl(cfd, F_GETFL);
+				flag |= O_NONBLOCK;
+				fcntl(cfd, F_SETFL, flag);
+
 				struct epoll_event temp;
 				temp.events = EPOLLIN;
-				//temp.events = EPOLLIN | EPOLLET;
+				// temp.events = EPOLLIN | EPOLLET;
 
 				temp.data.fd = cfd;
 				epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &temp);
 
-				// 打印客户端信息
 				char ip[64] = { 0 };
 				printf("New client IP: %s, Port: %d\n",
 					inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, ip, sizeof(ip)),
@@ -83,25 +82,25 @@ int main(int argc, const char* argv[])
 			}
 			else
 			{
-				// 已经连接的客户端
 				if (!all[i].events & EPOLLIN)
 				{
 					continue;
 				}
 
-				// EPOLLIN 事件
 				char buf[1024] = { 0 };
-				int len = recv(fd, buf, sizeof(buf), 0);
+				int len;
 
-				if (len == -1)
+				while ((len = recv(fd, buf, sizeof(buf), 0)) > 0)
 				{
-					perror("receive error");
-					exit(1);
+					write(STDOUT_FILENO, buf, len);
+
+					send(fd, buf, len, 0);
 				}
-				else if (len == 0)
+
+				if (len == 0)
 				{
-					printf("client disconnected ... \n");
-					epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+					printf("client disconncted!\n ");
+					ret = epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
 					if (ret == -1)
 					{
 						perror("epoll_ctl - error");
@@ -109,11 +108,18 @@ int main(int argc, const char* argv[])
 					}
 					close(fd);
 				}
-				else
+				else if (len == -1)
 				{
-					//printf(" revc buf: %s\n", buf);
-					write(STDOUT_FILENO, buf, len);
-					write(fd, buf, len);
+					if (errno == EAGAIN)
+					{
+						printf("buffer data is empty!\n");
+					}
+					else
+					{
+						printf("recv error! len == -1 \n");
+						perror("receive error");
+						exit(1);
+					}
 				}
 			}
 
